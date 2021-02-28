4af85b796f69a45ebc48a2f9e233dfc2c2bbca69
az365 <artemcarr@yandex.ru>
2021-02-28 23:16:28 +0300

    SchemaStream: select(), filter() methods added

--- streams/typed/schema_stream.py
+++ streams/typed/schema_stream.py
@@ -1,13 +1,17 @@
 try:  # Assume we're a sub-module in a package.
-    from streams import stream_classes as fx
-    from loggers import logger_classes
+    from streams import stream_classes as sm
+    from loggers import logger_classes as log
     from schema import schema_classes as sh
     from functions import all_functions as fs
+    from selection import selection_classes as sn
+    from utils import selection
 except ImportError:  # Apparently no higher-level package has been imported, fall back to a local import.
-    from .. import stream_classes as fx
-    from ...loggers import logger_classes
+    from .. import stream_classes as sm
+    from ...loggers import logger_classes as log
     from ...schema import schema_classes as sh
     from ...functions import all_functions as fs
+    from ...selection import selection_classes as sn
+    from ...utils import selection
 
 
 NAME_POS, TYPE_POS, HINT_POS = 0, 1, 2  # schema fields
@@ -67,18 +71,18 @@ def apply_schema_to_row(row, schema, skip_bad_values=False, logger=None):
                         field_name, c,
                         value, field_type,
                     )
-                    logger.log(msg=message, level=logger_classes.LoggingLevel.Error.value)
+                    logger.log(msg=message, level=log.LoggingLevel.Error.value)
                 if skip_bad_values:
                     if logger:
                         message = 'Skipping bad value in row:'.format(list(zip(row, schema)))
-                        logger.log(msg=message, level=logger_classes.LoggingLevel.Debug.value)
+                        logger.log(msg=message, level=log.LoggingLevel.Debug.value)
                     new_value = None
                 else:
                     message = 'Error in row: {}...'.format(str(list(zip(row, schema)))[:80])
                     if logger:
-                        logger.log(msg=message, level=logger_classes.LoggingLevel.Warning.value)
+                        logger.log(msg=message, level=log.LoggingLevel.Warning.value)
                     else:
-                        logger_classes.get_logger().show(message)
+                        log.get_logger().show(message)
                     raise e
             row[c] = new_value
         return row
@@ -86,7 +90,7 @@ def apply_schema_to_row(row, schema, skip_bad_values=False, logger=None):
         raise TypeError
 
 
-class SchemaStream(fx.RowStream):
+class SchemaStream(sm.RowStream):
     def __init__(
             self,
             data,
@@ -96,9 +100,9 @@ class SchemaStream(fx.RowStream):
             schema=None,
             source=None,
             context=None,
-            max_items_in_memory=fx.MAX_ITEMS_IN_MEMORY,
-            tmp_files_template=fx.TMP_FILES_TEMPLATE,
-            tmp_files_encoding=fx.TMP_FILES_ENCODING,
+            max_items_in_memory=sm.MAX_ITEMS_IN_MEMORY,
+            tmp_files_template=sm.TMP_FILES_TEMPLATE,
+            tmp_files_encoding=sm.TMP_FILES_ENCODING,
     ):
         super().__init__(
             check_rows(data, schema) if check else data,
@@ -125,6 +129,10 @@ class SchemaStream(fx.RowStream):
             self.schema,
             skip_errors,
         )
+
+    @staticmethod
+    def get_item_type():
+        return sn.it.ItemType.SchemaRow
 
     def get_schema(self):
         return self.schema
@@ -169,3 +177,52 @@ class SchemaStream(fx.RowStream):
             return self.schema.get_columns()
         elif isinstance(self.schema, (list, tuple)):
             return [c[0] for c in self.schema]
+
+    def get_schema_rows(self):
+        for r in self.get_items():
+            yield sh.SchemaRow(r, self.get_schema(), check=False)
+
+    def schematized_map(self, function, schema):
+        return self.__class__(
+            map(function, self.get_items()),
+            count=self.count,
+            less_than=self.count or self.less_than,
+            schema=schema,
+        )
+
+    def select(self, *args, **kwargs):
+        selection_description = sn.SelectionDescription.with_expressions(
+            *args, **kwargs,
+            target_item_type=self.get_item_type(), input_item_type=self.get_item_type(),
+            logger=self.get_logger(), selection_logger=self.get_selection_logger(),
+        )
+        selection_function = selection_description.get_mapper()
+        output_schema = selection_description.get_output_schema()
+        return self.schematized_map(
+            function=selection_function,
+            schema=output_schema,
+        )
+
+    def filter(self, *fields, **expressions):
+        expressions_list = [
+            (k, fs.equal(v) if isinstance(v, (str, int, float, bool)) else v)
+            for k, v in expressions.items()
+        ]
+        extended_filters_list = list(fields) + expressions_list
+
+        def filter_function(r):
+            for f in extended_filters_list:
+                if not selection.value_from_schema_row(r, f):
+                    return False
+            return True
+        props = self.get_meta()
+        props.pop('count')
+        filtered_items = filter(filter_function, self.get_items())
+        if self.is_in_memory():
+            filtered_items = list(filtered_items)
+            props['count'] = len(filtered_items)
+        return self.__class__(
+            filtered_items,
+            **props
+        )
+
