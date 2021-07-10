a9948778ce2bdc82e2404a58ebcce0d97372f00c
az365 <artemcarr@yandex.ru>
2021-07-10 21:39:12 +0300

    ExtendedLogger: set_file() method added

--- loggers/extended_logger.py
+++ loggers/extended_logger.py
@@ -1,4 +1,4 @@
-from typing import Union, Optional, Iterable, Any
+from typing import Union, Optional, Iterable, Any, NoReturn
 import logging
 
 try:  # Assume we're a sub-module in a package.
@@ -20,14 +20,23 @@ except ImportError:  # Apparently no higher-level package has been imported, fal
     from .progress_interface import ProgressInterface
     from .progress import Progress
 
-Level = Union[LoggingLevel, arg.DefaultArgument]
+Level = Union[LoggingLevel, int, arg.DefaultArgument]
+Name = str
+Count = Optional[int]
+Step = Union[Count, arg.DefaultArgument]
+Formatter = Union[str, logging.Formatter]
 Context = Optional[bs.ContextInterface]
+OptContext = Union[Context, arg.DefaultArgument]
+OptName = Union[Name, arg.DefaultArgument]
 BaseLogger = Union[LoggerInterface, Any]
 SubLoggers = Optional[Union[list, dict]]
+File = bs.AbstractNamed
+FileOrName = Union[File, Name]
 
 DEFAULT_LOGGER_NAME = 'default'
 DEFAULT_FORMATTER = '%(asctime)s - %(levelname)s - %(message)s'
 DEFAULT_LOGGING_LEVEL = LoggingLevel.get_default()
+DEFAULT_ENCODING = 'utf8'
 DEFAULT_LINE_LEN = 127
 LONG_LINE_LEN = 600
 TRUNCATED_SUFFIX = '..'
@@ -38,10 +47,12 @@ SPACE = ' '
 class BaseLoggerWrapper(bs.TreeItem, LoggerInterface):
     def __init__(
             self,
-            name: Union[str, arg.DefaultArgument] = arg.DEFAULT,
-            level: Level = arg.DEFAULT, formatter=arg.DEFAULT,
+            name: OptName = arg.DEFAULT,
+            level: Level = arg.DEFAULT,
+            formatter: Union[Formatter, arg.DefaultArgument] = arg.DEFAULT,
             loggers: SubLoggers = arg.DEFAULT,
             context: Context = None,
+            file: Optional[FileOrName] = None,
     ):
         name = arg.undefault(name, DEFAULT_LOGGER_NAME)
         level = arg.undefault(level, DEFAULT_LOGGING_LEVEL)
@@ -53,22 +64,28 @@ class BaseLoggerWrapper(bs.TreeItem, LoggerInterface):
         elif not arg.is_defined(loggers):
             loggers = dict()
         if name not in loggers:
-            level_value = level.get_value()
+            level_value = arg.get_value(level)
             base_logger = self.build_base_logger(name, level_value, formatter)
             loggers[name] = base_logger
         self._level = level
         super().__init__(name=name, children=loggers, context=context)
+        if file:
+            self.set_file(file)
 
     @staticmethod
-    def build_base_logger(name=DEFAULT_LOGGER_NAME, level=DEFAULT_LOGGING_LEVEL, formatter=DEFAULT_FORMATTER):
-        if isinstance(level, LoggingLevel):
-            level = level.get_value()
+    def build_base_logger(
+            name: Name = DEFAULT_LOGGER_NAME,
+            level: Level = logging.DEBUG,
+            formatter: Formatter = DEFAULT_FORMATTER,
+    ) -> BaseLogger:
+        level = arg.get_value(level)
         base_logger = logging.getLogger(name)
         base_logger.setLevel(level)
         if not base_logger.handlers:
             stream_handler = logging.StreamHandler()
-            stream_handler.setLevel(logging.DEBUG)
-            formatter = logging.Formatter(formatter)
+            stream_handler.setLevel(level)
+            if isinstance(formatter, str):
+                formatter = logging.Formatter(formatter)
             stream_handler.setFormatter(formatter)
             base_logger.addHandler(stream_handler)
         return base_logger
@@ -79,53 +96,91 @@ class BaseLoggerWrapper(bs.TreeItem, LoggerInterface):
     def get_level(self) -> LoggingLevel:
         return self._level
 
-    def log(self, msg, level, *args, **kwargs):
+    def get_handlers(self) -> list:
+        return self.get_base_logger().handlers
+
+    def add_handler(self, handler: logging.Handler, if_not_added: bool = True) -> LoggerInterface:
+        if handler not in self.get_handlers() or not if_not_added:
+            self.get_base_logger().addHandler(handler)
+        return self
+
+    def set_file(
+            self, file: Union[File, Name],
+            encoding: str = DEFAULT_ENCODING,
+            level: Level = DEFAULT_LOGGING_LEVEL,
+            formatter: Formatter = DEFAULT_FORMATTER,
+            if_not_added: bool = True,
+    ) -> LoggerInterface:
+        filename = arg.get_name(file)
+        level = arg.get_value(level)
+        is_added = False
+        for h in self.get_handlers():
+            if hasattr(h, 'baseFilename'):
+                if h.baseFilename.endswidth(filename):
+                    is_added = True
+                    break
+        if not (is_added and if_not_added):
+            file_handler = logging.FileHandler(filename, encoding=encoding)
+            file_handler.setLevel(level)
+            if isinstance(formatter, str):
+                formatter = logging.Formatter(formatter)
+            file_handler.setFormatter(formatter)
+            self.add_handler(file_handler)
+        return self
+
+    def log(self, msg: str, level: Level, *args, **kwargs) -> NoReturn:
         return self.get_base_logger().log(level=level, msg=msg, *args, **kwargs)
 
-    def debug(self, msg):
+    def debug(self, msg: str) -> NoReturn:
         self.log(msg=msg, level=LoggingLevel.Debug)
 
-    def info(self, msg):
+    def info(self, msg: str) -> NoReturn:
         self.log(msg=msg, level=LoggingLevel.Info)
 
-    def warning(self, msg):
+    def warning(self, msg: str) -> NoReturn:
         self.log(msg=msg, level=LoggingLevel.Warning)
 
-    def error(self, msg):
+    def error(self, msg: str) -> NoReturn:
         self.log(msg=msg, level=LoggingLevel.Error)
 
-    def critical(self, msg):
+    def critical(self, msg: str) -> NoReturn:
         self.log(msg=msg, level=LoggingLevel.Critical)
 
 
 class ExtendedLogger(BaseLoggerWrapper, ExtendedLoggerInterface):
     def __init__(
             self,
-            name: Union[str, arg.DefaultArgument] = arg.DEFAULT,
+            name: Union[Name, arg.DefaultArgument] = arg.DEFAULT,
             level: Level = arg.DEFAULT, formatter=arg.DEFAULT,
             max_line_len=arg.DEFAULT,
             context: Context = None,
+            file: Optional[FileOrName] = None,
     ):
         self.max_line_len = arg.undefault(max_line_len, DEFAULT_LINE_LEN)
         progress_trackers = dict()
         self.LoggingLevel = LoggingLevel
-        super().__init__(name=name, level=level, formatter=formatter, loggers=progress_trackers, context=context)
+        super().__init__(
+            name=name, level=level, formatter=formatter,
+            file=file, loggers=progress_trackers,
+            context=context,
+        )
 
     @staticmethod
     def is_common_logger() -> bool:
         return False
 
-    def put_into_context(self, check=False):
+    def put_into_context(self, check: bool = False) -> NoReturn:
         context = self.get_context()
         if context:
             if check:
                 registered_logger = context.get_logger(create_if_not_yet=False)
                 if registered_logger:
                     if hasattr(registered_logger, 'get_key_member_values'):
-                        assert registered_logger.get_key_member_values() == self.get_key_member_values()  # , 'Context already has logger registered'
+                        is_same_logger = registered_logger.get_key_member_values() == self.get_key_member_values()
+                        assert is_same_logger, 'Context already has logger registered'
             context.set_logger(self)
 
-    def get_new_progress(self, name, count=None, context=arg.DEFAULT) -> ProgressInterface:
+    def get_new_progress(self, name: Name, count: Count = None, context: OptContext = arg.DEFAULT) -> ProgressInterface:
         progress = Progress(
             name=name,
             count=count,
@@ -135,48 +190,54 @@ class ExtendedLogger(BaseLoggerWrapper, ExtendedLoggerInterface):
         self.add_child(progress, check=False)
         return progress
 
-    def progress(self, items: Iterable, name='Progress', count=None, step=arg.DEFAULT, context=arg.DEFAULT) -> Iterable:
+    def progress(
+            self, items: Iterable, name: Name = 'Progress',
+            count: Count = None, step: Step = arg.DEFAULT,
+            context: OptContext = arg.DEFAULT,
+    ) -> Iterable:
         return self.get_new_progress(name, count=count, context=context).iterate(items, step=step)
 
-    def get_selection_logger(self, name=arg.DEFAULT, **kwargs) -> SelectionLoggerInterface:
+    def get_selection_logger(self, name: OptName = arg.DEFAULT, **kwargs) -> Optional[SelectionLoggerInterface]:
         name = arg.undefault(name, SELECTION_LOGGER_NAME)
         selection_logger = self.get_child(name)
         if selection_logger:
             if kwargs:
                 selection_logger.set_meta(name, **kwargs)
         else:
-            self.reset_selection_logger(name, **kwargs)
+            selection_logger = self.reset_selection_logger(name, **kwargs)
         return selection_logger
 
-    def set_selection_logger(self, selection_logger: SelectionLoggerInterface, skip_errors=True):
+    def set_selection_logger(self, selection_logger: SelectionLoggerInterface, skip_errors: bool = True) -> NoReturn:
         try:
+            assert isinstance(selection_logger, bs.ContextualDataWrapper)
             self.add_child(selection_logger)
         except ValueError as e:
             if not skip_errors:
                 raise e
 
-    def reset_selection_logger(self, name: Union[str, arg.DefaultArgument] = arg.DEFAULT, **kwargs):
+    def reset_selection_logger(self, name: OptName = arg.DEFAULT, **kwargs) -> Optional[SelectionLoggerInterface]:
         name = arg.undefault(name, SELECTION_LOGGER_NAME)
         context = self.get_context()
         if context:
             selection_logger = context.get_new_selection_logger(name, **kwargs)
             if selection_logger:
                 self.set_selection_logger(selection_logger)
+                return selection_logger
 
-    def is_suitable_level(self, level: Union[int, LoggingLevel]) -> bool:
-        if hasattr(level, 'get_value'):
-            proposed_level_value = level.get_value()
-        else:
-            proposed_level_value = level
-        if hasattr(self.get_level(), 'get_value'):
-            selected_level_value = self.get_level().get_value()
-        else:
-            selected_level_value = self.get_level()
+    def is_suitable_level(self, level: Level) -> bool:
+        proposed_level_value = arg.get_value(level)
+        selected_level_value = arg.get_value(self.get_level())
         return proposed_level_value >= selected_level_value
 
-    def log(self, msg, level=arg.DEFAULT, logger=arg.DEFAULT, end=arg.DEFAULT, verbose=True, truncate=True):
+    def log(
+            self,
+            msg: Union[str, list, tuple], level: Level = arg.DEFAULT,
+            logger: Union[BaseLogger, arg.DefaultArgument] = arg.DEFAULT,
+            end: Union[str, arg.DefaultArgument] = arg.DEFAULT,
+            verbose: bool = True, truncate: bool = True,
+    ) -> NoReturn:
         level = arg.undefault(level, LoggingLevel.Info if verbose else LoggingLevel.Debug)
-        logger = arg.undefault(logger, self.get_base_logger())
+        logger = arg.delayed_undefault(logger, self.get_base_logger)
         if isinstance(msg, (list, tuple)):
             msg = self.format_message(*msg)
         if not isinstance(level, LoggingLevel):
@@ -188,7 +249,11 @@ class ExtendedLogger(BaseLoggerWrapper, ExtendedLoggerInterface):
         if verbose and not self.is_suitable_level(level):
             self.show(msg, end=end, truncate=truncate)
 
-    def format_message(self, *messages, max_len=arg.DEFAULT, truncate=True):
+    def format_message(
+            self, *messages,
+            max_len: Union[int, arg.DefaultArgument] = arg.DEFAULT,
+            truncate: bool = True,
+    ) -> str:
         messages = arg.update(messages)
         max_len = arg.undefault(max_len, self.max_line_len)
         message = SPACE.join([str(m) for m in messages])
@@ -196,11 +261,15 @@ class ExtendedLogger(BaseLoggerWrapper, ExtendedLoggerInterface):
             message = message[:max_len - 2] + TRUNCATED_SUFFIX
         return message
 
-    def clear_line(self):
+    def clear_line(self) -> NoReturn:
         print('\r', end='')
         print(SPACE * self.max_line_len, end='\r')
 
-    def show(self, *messages, end=arg.DEFAULT, clear_before=True, truncate=True):
+    def show(
+            self, *messages,
+            end: Union[str, arg.DefaultArgument] = arg.DEFAULT,
+            clear_before: bool = True, truncate: bool = True,
+    ) -> NoReturn:
         message = self.format_message(
             *messages,
             max_len=LONG_LINE_LEN if end == '\n' else self.max_line_len,
--- loggers/extended_logger_interface.py
+++ loggers/extended_logger_interface.py
@@ -1,15 +1,32 @@
 from abc import ABC, abstractmethod
 from enum import Enum
+from typing import Optional, Iterable, Union, Any, NoReturn
 import logging
 
 try:  # Assume we're a sub-module in a package.
     from utils import arguments as arg
+    from base.interfaces.base_interface import BaseInterface
     from base.interfaces.sourced_interface import SourcedInterface
     from loggers.logger_interface import LoggerInterface
 except ImportError:  # Apparently no higher-level package has been imported, fall back to a local import.
     from ..utils import arguments as arg
+    from ..base.interfaces.base_interface import BaseInterface
     from ..base.interfaces.sourced_interface import SourcedInterface
     from .logger_interface import LoggerInterface
+
+BaseLogger = Union[LoggerInterface, Any]
+Context = Optional[BaseInterface]
+Name = str
+Count = Optional[int]
+Step = Union[Count, arg.DefaultArgument]
+Formatter = Union[str, logging.Formatter]
+OptContext = Union[Context, arg.DefaultArgument]
+OptName = Union[Name, arg.DefaultArgument]
+File = BaseInterface
+
+DEFAULT_LOGGER_NAME = 'default'
+DEFAULT_FORMATTER = '%(asctime)s - %(levelname)s - %(message)s'
+DEFAULT_ENCODING = 'utf8'
 
 
 class LoggingLevel(Enum):
@@ -19,10 +36,10 @@ class LoggingLevel(Enum):
     Error = logging.ERROR
     Critical = logging.CRITICAL
 
-    def get_value(self):
+    def get_value(self) -> int:
         return self.value
 
-    def get_name(self):
+    def get_name(self) -> str:
         if self == LoggingLevel.Debug:
             return 'debug'
         elif self == LoggingLevel.Info:
@@ -34,7 +51,7 @@ class LoggingLevel(Enum):
         elif self == LoggingLevel.Critical:
             return 'critical'
 
-    def get_method_name(self):
+    def get_method_name(self) -> str:
         return self.get_name()
 
     @classmethod
@@ -42,23 +59,71 @@ class LoggingLevel(Enum):
         return cls.Warning
 
 
+Level = Union[LoggingLevel, int, arg.DefaultArgument]
+
+DEFAULT_LOGGING_LEVEL = LoggingLevel.get_default()
+
+
 class ExtendedLoggerInterface(SourcedInterface, LoggerInterface, ABC):
     @staticmethod
+    @abstractmethod
     def is_common_logger() -> bool:
         pass
 
     @abstractmethod
-    def progress(self, items, name='Progress', count=None, step=arg.DEFAULT, context=arg.DEFAULT):
+    def get_base_logger(self) -> BaseLogger:
         pass
 
     @abstractmethod
-    def get_new_progress(self, name, **kwargs):
+    def get_level(self) -> LoggingLevel:
         pass
 
     @abstractmethod
-    def get_selection_logger(self, **kwargs):
+    def get_handlers(self) -> list:
         pass
 
     @abstractmethod
-    def set_selection_logger(self, selection_logger, skip_errors=True):
+    def add_handler(self, handler: logging.Handler, if_not_added: bool = True) -> LoggerInterface:
+        if handler not in self.get_handlers() or not if_not_added:
+            self.get_base_logger().addHandler(handler)
+        return self
+
+    @abstractmethod
+    def set_file(
+            self, file: Union[File, Name],
+            encoding: str = DEFAULT_ENCODING,
+            level: Level = DEFAULT_LOGGING_LEVEL,
+            formatter: Formatter = DEFAULT_FORMATTER,
+            if_not_added: bool = True,
+    ) -> LoggerInterface:
+        pass
+
+    @abstractmethod
+    def progress(
+            self, items: Iterable, name: Name = 'Progress',
+            count: Count = None, step: Step = arg.DEFAULT,
+            context: OptContext = arg.DEFAULT,
+    ) -> Iterable:
+        pass
+
+    @abstractmethod
+    def get_new_progress(self, name: Name, count: Count = None, context: OptContext = arg.DEFAULT):
+        pass
+
+    @abstractmethod
+    def get_selection_logger(self, name: OptName = arg.DEFAULT, **kwargs):
+        pass
+
+    @abstractmethod
+    def set_selection_logger(self, selection_logger, skip_errors: bool = True) -> NoReturn:
+        pass
+
+    @abstractmethod
+    def log(
+            self,
+            msg: Union[str, list, tuple], level: Level = arg.DEFAULT,
+            logger: Union[BaseLogger, arg.DefaultArgument] = arg.DEFAULT,
+            end: Union[str, arg.DefaultArgument] = arg.DEFAULT,
+            verbose: bool = True, truncate: bool = True,
+    ) -> NoReturn:
         pass
