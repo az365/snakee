ae38146f9c181aabf9565a72668b1f62ad60d5b0
az365 <artemcarr@yandex.ru>
2021-07-18 06:51:39 +0300

    logic_functions: conditioned and simple modes added

--- functions/logic_functions.py
+++ functions/logic_functions.py
@@ -9,32 +9,53 @@ except ImportError:  # Apparently no higher-level package has been imported, fal
 def maybe(*conditions) -> Callable:
     conditions = arg.update(conditions)
 
-    def func(value) -> bool:
+    def func_conditioned(value) -> bool:
         for c in conditions:
             if c(value):
                 return True
         return False
-    return func
+
+    def func_simple(*values) -> bool:
+        return max(map(bool, values))
+
+    if conditions:
+        return func_conditioned
+    else:
+        return func_simple
 
 
 def always(*conditions) -> Callable:
     conditions = arg.update(conditions)
 
-    def func(value) -> bool:
+    def func_conditioned(value) -> bool:
         for c in conditions:
             if not c(value):
                 return False
         return True
-    return func
+
+    def func_simple(*values) -> bool:
+        values = arg.update(values)
+        return min(map(bool, values))
+
+    if conditions:
+        return func_conditioned
+    else:
+        return func_simple
 
 
 def never(*conditions) -> Callable:
     conditions = arg.update(conditions)
 
-    def func(value) -> bool:
+    def func_conditioned(value) -> bool:
         for c in conditions:
             if c(value):
                 return False
         return True
-    return func
 
+    def func_simple(value) -> bool:
+        return not value
+
+    if conditions:
+        return func_conditioned
+    else:
+        return func_simple
