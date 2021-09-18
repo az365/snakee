8d23191f82ba3ac3dca62eb5b59adeb95fc23645
az365 <artemcarr@yandex.ru>
2021-09-18 07:51:47 +0300

    FolderType: redundant comments removed

--- connectors/filesystem/folder_type.py
+++ connectors/filesystem/folder_type.py
@@ -9,34 +9,9 @@ class FolderType(ClassType):
     LocalFolder = 'LocalFolder'
     FileMask = 'FileMask'
 
-    # def get_name(self):
-    #     return self.value
-    #
-    # def get_value(self):
-    #     return self.value
-    #
-    # @staticmethod
-    # def _get_dict_classes():
-    #     return {FolderType.LocalFolder: LocalFolder, FolderType.FileMask: FileMask}
-    #
-    # def get_class(self, skip_missing=False):
-    #     found_class = self._get_dict_classes().get(self)
-    #     if found_class:
-    #         return found_class
-    #     elif not skip_missing:
-    #         raise ValueError('class for {} not supported'.format(self))
-
     @staticmethod
     def detect_by_name(name: str):
         if '*' in name:
             return FolderType.FileMask
         else:
             return FolderType.LocalFolder
-
-    # def get_type(self) -> ClassType:
-    #     return self
-
-    # @classmethod
-    # def prepare(cls):
-    #     super().prepare()
-    #     cls.set_dict_classes({FolderType.LocalFolder: LocalFolder, FolderType.FileMask: FileMask})
