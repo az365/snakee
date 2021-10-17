1efdb291581c3d69f1d6de41a58c3912a790335b
az365 <artemcarr@yandex.ru>
2021-10-17 23:11:13 +0300

    IterableStreamMixin class added

--- streams/stream_classes.py
+++ streams/stream_classes.py
@@ -20,6 +20,7 @@ try:  # Assume we're a sub-module in a package.
     from streams.abstract.wrapper_stream import WrapperStream
     from streams.mixin.columnar_mixin import ColumnarMixin
     from streams.mixin.convert_mixin import ConvertMixin
+    from streams.mixin.iterable_mixin import IterableStreamMixin
     from streams.regular.any_stream import AnyStream
     from streams.regular.line_stream import LineStream
     from streams.regular.row_stream import RowStream
@@ -45,6 +46,7 @@ except ImportError:  # Apparently no higher-level package has been imported, fal
     from .abstract.wrapper_stream import WrapperStream
     from .mixin.columnar_mixin import ColumnarMixin
     from .mixin.convert_mixin import ConvertMixin
+    from .mixin.iterable_mixin import IterableStreamMixin
     from .regular.any_stream import AnyStream
     from .regular.line_stream import LineStream
     from .regular.row_stream import RowStream
