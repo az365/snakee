deccf979c4fc43cf41783d7c2abd906b4b117513
az365 <artemcarr@yandex.ru>
2021-10-24 23:29:46 +0300

    ConnectorInterface: docstrings added

--- connectors/interfaces/connector_interface.py
+++ connectors/interfaces/connector_interface.py
@@ -23,106 +23,167 @@ CHUNK_SIZE = 8192
 
 
 class ConnectorInterface(SourcedInterface, ABC):
-    @staticmethod
-    def is_connector() -> bool:
-        return True
-
-    @staticmethod
     @abstractmethod
-    def is_storage() -> bool:
-        pass
-
-    @staticmethod
-    @abstractmethod
-    def is_folder() -> bool:
-        pass
-
-    @staticmethod
-    @abstractmethod
-    def has_hierarchy() -> bool:
-        pass
-
-    @abstractmethod
-    def get_storage(self) -> OptionalParent:
-        pass
-
-    @abstractmethod
-    def get_logger(self, skip_missing=True, create_if_not_yet=True) -> Logger:
-        pass
-
-    @abstractmethod
-    def log(self, msg, level=AUTO, end=AUTO, verbose=True) -> NoReturn:
-        pass
-
-    @abstractmethod
-    def get_new_progress(self, name, count=None, context=AUTO) -> ProgressInterface:
-        pass
-
-    @abstractmethod
-    def get_path_prefix(self) -> str:
-        pass
-
-    @abstractmethod
-    def get_path_delimiter(self) -> str:
+    def get_config_dict(self) -> dict:
+        """Returns dict with connector setting for save into json-config file or object."""
         pass
 
     @abstractmethod
     def get_path(self) -> str:
+        """Returns full path to file/object as string, including prefix."""
         pass
 
     @abstractmethod
     def get_path_as_list(self) -> list:
+        """Returns bread crumbs of path to file/object as list."""
         pass
 
     @abstractmethod
-    def get_config_dict(self) -> dict:
+    def get_path_prefix(self) -> str:
+        """Returns path prefix string for parent database, storage or filesystem,
+        i.e. s3u:// for s3 storage or ~/ for local job folder.
+        """
+        pass
+
+    @abstractmethod
+    def get_path_delimiter(self) -> str:
+        """Returns path delimiter symbols for parent database, storage or filesystem,
+        i.e. / for Linux or s3, \\ for Windows, . for SQL schemeS and tables.
+        """
         pass
 
     @abstractmethod
     def get_parent(self) -> OptionalParent:
+        """Returns parent connector (i.e. folder for file) if provided."""
         pass
 
     @abstractmethod
     def set_parent(self, parent: SourcedInterface, reset: bool = False, inplace: bool = True):
+        """Remember provided connector (or context) object as parent of this connector
+        and register this connector as child of provided parend after initialization.
+        """
         pass
 
     @abstractmethod
     def get_items(self) -> Iterable:
+        """Returns child connectors (for HierarchicConnector) or data items (for LeafConnector instances)"""
         pass
 
     @abstractmethod
     def get_children(self) -> dict:
+        """Returns dict with names and connectors of all child connectors"""
         pass
 
     @abstractmethod
     def get_child(self, name: str) -> SourcedInterface:
+        """Find children connector object by name."""
         pass
 
     @abstractmethod
     def add_child(self, child: SourcedInterface):
+        """Remember new child connector (i.e. register new file in folder)."""
         pass
 
     @abstractmethod
     def forget_child(self, child_or_name: Union[SourcedInterface, str], skip_errors=False):
+        """Closes related connection(s) and lets SnakeeContext forget the link to this connector."""
         pass
 
     @abstractmethod
-    def is_leaf(self) -> bool:
+    def get_storage(self) -> OptionalParent:
+        """Returns parent storage of local file or s3 object."""
         pass
 
     @abstractmethod
-    def is_root(self) -> bool:
+    def get_context(self):
+        """Returns SnakeeContext if available."""
+        pass
+
+    @abstractmethod
+    def set_context(self, context, reset=False):
+        """Using for provide context object to connector after initialization."""
         pass
 
     @staticmethod
     @abstractmethod
     def is_context() -> bool:
+        """Check is this object is SnakeeContext singleton."""
+        pass
+
+    @staticmethod
+    def is_connector() -> bool:
+        """Check is this object the child of AbstractConnector.
+        Can be used instead of isinstance(obj, AbstractConnector).
+        """
+        return True
+
+    @staticmethod
+    @abstractmethod
+    def is_storage() -> bool:
+        """Check is this connector the child of AbstractStorage.
+        Can be used instead of isinstance(obj, AbstractStorage).
+        """
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def is_folder() -> bool:
+        """Check is this connector the child of AbstractFolder.
+        Can be used instead of isinstance(obj, AbstractFolder).
+        """
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def has_hierarchy() -> bool:
+        """Check is this connector the child of HierarchicConnector.
+        Can be used instead of isinstance(obj, HierarchicConnector).
+        """
         pass
 
     @abstractmethod
-    def get_context(self):
+    def is_leaf(self) -> bool:
+        """Check is this connector the child of LeafConnector.
+        Can be used instead of isinstance(obj, LeafConnector).
+        """
         pass
 
     @abstractmethod
-    def set_context(self, context, reset=False):
+    def is_root(self) -> bool:
+        """Check is this object is the root of current connectors hierarchy,
+        it means that this root node has not parent in tree graph.
+        """
+        pass
+
+    @abstractmethod
+    def get_logger(self, skip_missing=True, create_if_not_yet=True) -> Logger:
+        """Returns current common (default) logger.
+
+        :param skip_missing: do not raise errors if common logger is not available or not set.
+        :param create_if_not_yet: create new logger if current logger is not set.
+        :return: ExtendedLogger from SnakeeContext if context is defined, otherwise FallbackLogger.
+        """
+        pass
+
+    @abstractmethod
+    def log(self, msg, level=AUTO, end=AUTO, verbose=True) -> NoReturn:
+        """Log message with using current common logger
+
+        :param msg: any text message as str
+        :param level: int LoggingLevel: 10 Debug, 20 Info, 30 Warning, 40 Error, 50 Critical
+        :param end: ending symbol for STDOUT line, can be \n (write line permanently) or \r (temporary line to rewrite)
+        :param verbose: flag to show message in STDOUT, otherwise write log line in logger file only.
+        :return: nothing
+        """
+        pass
+
+    @abstractmethod
+    def get_new_progress(self, name, count=None, context=AUTO) -> ProgressInterface:
+        """Initialize new progress-bar using ExtendedLogger
+
+        :param name: name of Progress object corresponding title message for progress-bar
+        :param count: expected count of items for count percent of execution over iterator
+        :param context: SnakeeContext object for obtain common logger and settings
+        :return: Progress object receiving iterable object for measure and show progress of iteration
+        """
         pass
