b44a760521d01ffb7578b6c573691fba83367cc0
az365 <artemcarr@yandex.ru>
2021-11-28 17:32:55 +0300

    utils.enum: isinstance() method updated

--- utils/enum.py
+++ utils/enum.py
@@ -233,8 +233,10 @@ class ClassType(DynamicEnum):
         if builder:
             return builder(*args, **kwargs)
 
-    def isinstance(self, obj) -> bool:
-        return isinstance(obj, self.get_class())
+    def isinstance(self, obj) -> Optional[bool]:
+        native_class = self.get_class(skip_missing=True)
+        if arg.is_defined(native_class, check_name=False):
+            return isinstance(obj, native_class)
 
     @classmethod
     def detect(cls, obj, default: Union[Optional[DynamicEnum], Name] = None) -> EnumItem:
