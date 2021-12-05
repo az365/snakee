32a04e79b2b4835dae0223a55c2d80bba885daac
az365 <artemcarr@yandex.ru>
2021-12-05 04:45:04 +0300

    base_classes: enum classes import added

--- base/base_classes.py
+++ base/base_classes.py
@@ -13,6 +13,7 @@ try:  # Assume we're a sub-module in a package.
     from base.abstract.contextual_data import ContextualDataWrapper
     from base.abstract.simple_data import SimpleDataWrapper
     from base.abstract.tree_item import TreeItem
+    from base.enum import EnumItem, DynamicEnum, ClassType, SubclassesType
 except ImportError:  # Apparently no higher-level package has been imported, fall back to a local import.
     from ..utils import arguments as arg
     from .interfaces.base_interface import BaseInterface
@@ -21,9 +22,11 @@ except ImportError:  # Apparently no higher-level package has been imported, fal
     from .interfaces.data_interface import SimpleDataInterface, ContextualDataInterface
     from .interfaces.tree_interface import TreeInterface
     from .interfaces.context_interface import ContextInterface
+    from .abstract.abstract_base import AbstractBaseObject
     from .abstract.named import AbstractNamed
     from .abstract.sourced import Sourced
     from .abstract.contextual import Contextual
     from .abstract.contextual_data import ContextualDataWrapper
     from .abstract.simple_data import SimpleDataWrapper
     from .abstract.tree_item import TreeItem
+    from .enum import EnumItem, DynamicEnum, ClassType, SubclassesType
