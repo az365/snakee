fd380fe0e3854a60d1fe532be27071c9a0e728a6
az365 <artemcarr@yandex.ru>
2021-12-12 20:10:03 +0300

    refactoring: test_dates moved from utils.tests to functions.tests

--- functions/func_tests.py
+++ functions/func_tests.py
@@ -1,11 +1,11 @@
 try:  # Assume we're a sub-module in a package.
-    from utils.tests import tests_mappers
+    from functions.tests import test_dates
 except ImportError:  # Apparently no higher-level package has been imported, fall back to a local import.
-    from ..utils.tests import tests_mappers
+    from .tests import test_dates
 
 
 def main():
-    tests_mappers.main()
+    test_dates.main()
 
 
 if __name__ == '__main__':
--- tests.py
+++ tests.py
@@ -2,12 +2,14 @@ try:  # Assume we're a sub-module in a package.
     from streams import stream_tests
     from utils import utils_tests
     from fields import fields_test
+    from functions import func_tests
     from series import series_tests
     from connectors import conn_test
 except ImportError:  # Apparently no higher-level package has been imported, fall back to a local import.
     from .streams import stream_tests
     from .utils import utils_tests
     from .fields import fields_test
+    from .functions import func_tests
     from .series import series_tests
     from .connectors import conn_test
 
@@ -17,6 +19,7 @@ def main():
     series_tests.main()
     utils_tests.main()
     fields_test.main()
+    func_tests.main()
     conn_test.main()
 
 
