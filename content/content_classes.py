085d00a721e0a146eeb8a7bcfda99b513a795ac2
az365 <artemcarr@yandex.ru>
2022-01-23 04:18:52 +0300

    content_classes, item_classes modules added

---
+++ content/content_classes.py
@@ -0,0 +1,12 @@
+try:  # Assume we're a submodule in a package.
+    from content.fields.field_classes import *
+    from content.format.format_classes import *
+    from content.items.item_classes import *
+    from content.selection.selection_classes import *
+    from content.struct.flat_struct import *
+except ImportError:  # Apparently no higher-level package has been imported, fall back to a local import.
+    from .fields.field_classes import *
+    from .format.format_classes import *
+    from .items.item_classes import *
+    from .selection.selection_classes import *
+    from .struct.flat_struct import *
