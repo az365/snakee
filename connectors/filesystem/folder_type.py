c535576f2b47767c823be68fd8cea254ee18836e
az365 <artemcarr@yandex.ru>
2022-02-13 21:10:12 +0300

    FolderType: import fixed

--- connectors/filesystem/folder_type.py
+++ connectors/filesystem/folder_type.py
@@ -2,8 +2,8 @@ try:  # Assume we're a submodule in a package.
     from base.classes.enum import ClassType
     from utils.decorators import deprecated_with_alternative
 except ImportError:  # Apparently no higher-level package has been imported, fall back to a local import.
-    from base.classes.enum import ClassType
-    from utils.decorators import deprecated_with_alternative
+    from ...base.classes.enum import ClassType
+    from ...utils.decorators import deprecated_with_alternative
 
 
 # @deprecated_with_alternative(ConnType)
