20830b17d9ffdbbf2849fdd9b0aab633ca2f4001
az365 <artemcarr@yandex.ru>
2022-02-19 02:54:09 +0300

    content_interfaces module added

---
+++ content/content_interfaces.py
@@ -0,0 +1,29 @@
+from typing import Callable, Union, Any
+
+try:  # Assume we're a submodule in a package.
+    from base.classes.auto import Auto
+    from utils.algo import JoinType
+    from content.fields.field_interface import FieldInterface, FieldType
+    from content.format.format_interface import ContentFormatInterface, Compress
+    from content.items.item_classes import *
+    from content.struct.struct_interface import StructInterface, StructMixinInterface
+    from content.struct.struct_row_interface import StructRowInterface
+except ImportError:  # Apparently no higher-level package has been imported, fall back to a local import.
+    from ..base.classes.auto import Auto
+    from ..utils.algo import JoinType
+    from .fields.field_classes import FieldInterface, FieldType
+    from .format.format_interface import ContentFormatInterface, Compress
+    from .items.item_classes import *
+    from .struct.struct_interface import StructInterface, StructMixinInterface
+    from .struct.struct_row_interface import StructRowInterface
+
+Field = Union[FieldID, FieldInterface]
+Struct = StructInterface
+Group = Union[Struct, Array]
+FieldOrStruct = Union[FieldInterface, StructInterface]
+FieldOrGroup = Union[Field, Group]
+UniKey = Union[StructInterface, Array, FieldID, Auto, Callable]
+StructRow = StructRowInterface
+RegularItem = Union[SimpleItem, StructRow]
+Item = Union[Any, RegularItem]
+How = Union[JoinType, str]
