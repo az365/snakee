e93877f4a21666a2222acc7a3a23e3644434b957
a z <artemcarr@yandex.ru>
2022-04-17 04:42:37 +0300

    StreamType.isinstance(): by_type argument added

--- streams/stream_type.py
+++ streams/stream_type.py
@@ -1,15 +1,13 @@
-import inspect
+from inspect import isclass
+
+try:  # Assume we're a submodule in a package.
+    from base.classes.enum import ClassType, Auto, AUTO
+except ImportError:  # Apparently no higher-level package has been imported, fall back to a local import.
+    from base.classes.enum import ClassType, Auto, AUTO
 
 MAX_ITEMS_IN_MEMORY = 5000000
 TMP_FILES_TEMPLATE = 'stream_{}.tmp'
 TMP_FILES_ENCODING = 'utf8'
-
-try:  # Assume we're a sub-module in a package.
-    from utils import arguments as arg
-    from base.classes.enum import ClassType
-except ImportError:  # Apparently no higher-level package has been imported, fall back to a local import.
-    from ..utils import arguments as arg
-    from ..base.classes.enum import ClassType
 
 DICT_METHOD_SUFFIX = dict(
     AnyStream='any_stream',
@@ -37,21 +35,21 @@ class StreamType(ClassType):
         global DICT_METHOD_SUFFIX
         return DICT_METHOD_SUFFIX.get(self.get_name())
 
-    def stream(self, data, source=arg.AUTO, context=arg.AUTO, *args, **kwargs):
-        if arg.is_defined(source):
+    def stream(self, data, source=AUTO, context=AUTO, *args, **kwargs):
+        if Auto.is_defined(source):
             kwargs['source'] = source
-        if arg.is_defined(context):
+        if Auto.is_defined(context):
             kwargs['context'] = context
         stream_class = self.get_class()
         return stream_class(data, *args, **kwargs)
 
     @classmethod
-    def detect(cls, obj, default=arg.AUTO) -> ClassType:
+    def detect(cls, obj, default=AUTO) -> ClassType:
         if isinstance(obj, StreamType):
             return obj
         elif isinstance(obj, str):
             name = obj
-        elif inspect.isclass(obj):
+        elif isclass(obj):
             name = obj.__name__
         else:
             name = obj.__class__.__name__
@@ -63,7 +61,7 @@ class StreamType(ClassType):
                     stream_type_name = '{}Stream'.format(item_type_name)
                     stream_type_obj = cls.find_instance(stream_type_name)
                 if stream_type_obj is None:
-                    stream_type_obj = arg.delayed_acquire(default, cls.get_default)
+                    stream_type_obj = Auto.delayed_acquire(default, cls.get_default)
                 return stream_type_obj
         return StreamType(name)
 
@@ -81,11 +79,11 @@ class StreamType(ClassType):
         if hasattr(stream_class, 'get_item_type'):
             return stream_class.get_item_type()
 
-    def isinstance(self, stream) -> bool:
-        if hasattr(stream, 'get_stream_type'):
+    def isinstance(self, stream, by_type: bool = True) -> bool:
+        if by_type and hasattr(stream, 'get_stream_type'):
             return stream.get_stream_type() == self
         else:
-            return super().isinstance(stream)
+            return super().isinstance(stream, by_type=by_type)
 
 
 StreamType.prepare()
