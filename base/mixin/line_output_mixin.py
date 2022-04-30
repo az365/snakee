dcb65de2552e5623611d56957106466b48e6a048
a z <artemcarr@yandex.ru>
2022-04-30 05:42:39 +0300

    DefaultDisplay class added

--- base/mixin/line_output_mixin.py
+++ base/mixin/line_output_mixin.py
@@ -1,113 +1,72 @@
 from abc import ABC
-from typing import Optional, Callable, Iterable, Generator, Sequence, Union
+from typing import Optional, Iterable, Generator, Sequence, Union
 
 try:  # Assume we're a submodule in a package.
     from base.classes.typing import AUTO, Auto, AutoCount, Class
+    from base.classes.display import DefaultDisplay, PREFIX_FIELD, DEFAULT_ROWS_COUNT
     from base.functions.arguments import get_name, get_value
     from base.constants.chars import DEFAULT_LINE_LEN, REPR_DELIMITER, SMALL_INDENT, EMPTY
     from base.interfaces.line_output_interface import LineOutputInterface, AutoOutput, LoggingLevel
 except ImportError:  # Apparently no higher-level package has been imported, fall back to a local import.
     from ..classes.typing import AUTO, Auto, AutoCount, Class
+    from ..classes.display import DefaultDisplay, PREFIX_FIELD, DEFAULT_ROWS_COUNT
     from ..functions.arguments import get_name, get_value
     from ..constants.chars import DEFAULT_LINE_LEN, REPR_DELIMITER, SMALL_INDENT, EMPTY
     from ..interfaces.line_output_interface import LineOutputInterface, AutoOutput, LoggingLevel
 
-DEFAULT_ROWS_COUNT = 10
-DEFAULT_INT_WIDTH, DEFAULT_FLOAT_WIDTH = 7, 12
-PREFIX_FIELD = 'prefix'
+_display = DefaultDisplay()
 
 
 class LineOutputMixin(LineOutputInterface, ABC):
-    def get_output(self, output: AutoOutput = AUTO) -> Optional[Class]:
-        if Auto.is_defined(output) and not isinstance(output, LoggingLevel):
-            return output
-        if hasattr(self, 'get_logger'):
-            logger = self.get_logger()
-            if Auto.is_defined(logger):
-                return logger
-        elif Auto.is_auto(output):
-            return print
+    @classmethod
+    def get_display(cls, display: Optional[DefaultDisplay] = None) -> DefaultDisplay:
+        if isinstance(display, DefaultDisplay):
+            return display
+        else:
+            global _display
+            return _display
 
+    @classmethod
+    def set_display(cls, display: DefaultDisplay):
+        cls.set_display_inplace(display)
+        return cls
+
+    @classmethod
+    def set_display_inplace(cls, display: DefaultDisplay):
+        global _display
+        _display = display
+
+    display = property(get_display, set_display_inplace)
+
+    # @deprecated
+    @classmethod
+    def get_output(cls, output: AutoOutput = AUTO) -> Optional[Class]:
+        return cls.get_display().get_output(output)
+
+    # @deprecated
     def output_line(self, line: str, output: AutoOutput = AUTO) -> None:
-        logger_kwargs = dict(stacklevel=None)
-        if isinstance(output, LoggingLevel):
-            logger_kwargs['level'] = output
-            output = AUTO
-        if Auto.is_auto(output):
-            if hasattr(self, 'log'):
-                return self.log(msg=line, **logger_kwargs)
-            else:
-                output = self.get_output()
-        if isinstance(output, Callable):
-            return output(line)
-        elif output:
-            if hasattr(output, 'output_line'):
-                try:
-                    return output.output_line(line=line)
-                except TypeError as e:
-                    raise TypeError(f'{output}.output_line({line}): {e}')
-                return output.output_line(line)
-            elif hasattr(output, 'log'):
-                return output.log(msg=line, **logger_kwargs)
-            else:
-                raise TypeError('Expected Output, Logger or Auto, got {}'.format(output))
+        return self.get_display().output_line(line, output=output)
 
+    # @deprecated
     def output_blank_line(self, output: AutoOutput = AUTO) -> None:
         self.output_line(EMPTY, output=output)
 
+    # @deprecated
     @classmethod
     def _get_formatter(cls, columns: Sequence, delimiter: str = REPR_DELIMITER) -> str:
-        meta_description_placeholders = list()
-        for name, size in zip(cls._get_column_names(columns), cls._get_column_lens(columns)):
-            if size is None:
-                formatter = name
-            elif size:
-                formatter = '{name}:{size}'.format(name=name, size=size)
-            else:
-                formatter = EMPTY
-            meta_description_placeholders.append('{open}{f}{close}'.format(open='{', f=formatter, close='}'))
-        return delimiter.join(meta_description_placeholders)
+        return cls.get_display()._get_formatter(columns, delimiter=delimiter)
 
-    @staticmethod
-    def _get_column_names(columns: Iterable, ex: Union[str, Sequence, None] = None) -> Generator:
-        if ex is None:
-            ex = []
-        elif isinstance(ex, str):
-            ex = [ex]
-        for c in columns:
-            if c in ex:
-                yield ''
-            elif isinstance(c, (int, str)):
-                yield c
-            elif isinstance(c, Sequence):
-                yield c[0]
-            else:
-                raise get_name(c)
+    # @deprecated
+    @classmethod
+    def _get_column_names(cls, columns: Iterable, ex: Union[str, Sequence, None] = None) -> Generator:
+        return cls.get_display()._get_column_names(columns, ex=ex)
 
-    @staticmethod
-    def _get_column_lens(columns: Iterable, max_len: Optional[int] = None) -> Generator:
-        for c in columns:
-            if isinstance(c, (int, str)):
-                yield max_len
-            elif isinstance(c, Sequence):
-                if len(c) > 1:
-                    if isinstance(c[1], int):
-                        yield c[1]
-                    elif c[1] == int:
-                        yield DEFAULT_INT_WIDTH
-                    elif c[1] == float:
-                        yield DEFAULT_FLOAT_WIDTH
-                    else:  # c == str
-                        yield max_len
-                else:
-                    yield max_len
-            elif hasattr(c, 'get_max_len'):
-                yield c.get_max_len()
-            elif hasattr(c, 'get_repr'):
-                yield c.get_repr().get_max_len()
-            else:
-                yield max_len
+    # @deprecated
+    @classmethod
+    def _get_column_lens(cls, columns: Iterable, max_len: Optional[int] = None) -> Generator:
+        return cls.get_display()._get_column_lens(columns, max_len=max_len)
 
+    # @deprecated
     @classmethod
     def _get_cropped_record(
             cls,
@@ -128,6 +87,7 @@ class LineOutputMixin(LineOutputInterface, ABC):
             values = [str(v) if k not in ex else '' for k, v in zip(names, item)]
         return {c: str(v)[:s] for c, v, s in zip(names, values, lens)}
 
+    # @deprecated
     @classmethod
     def _get_columnar_lines(
             cls,
@@ -139,27 +99,23 @@ class LineOutputMixin(LineOutputInterface, ABC):
             delimiter: str = REPR_DELIMITER,
             max_len: int = DEFAULT_LINE_LEN,
     ) -> Generator:
-        count = Auto.acquire(count, DEFAULT_ROWS_COUNT)
-        formatter = cls._get_formatter(columns=columns, delimiter=delimiter)
-        if with_title:
-            column_names = cls._get_column_names(columns, ex=PREFIX_FIELD)
-            title_record = cls._get_cropped_record(column_names, columns=columns, max_len=max_len, ex=PREFIX_FIELD)
-            yield formatter.format(**{k: v.upper() for k, v in title_record.items()})
-        for n, r in enumerate(records):
-            if count is not None and n >= count:
-                break
-            if prefix and PREFIX_FIELD not in r:
-                r[PREFIX_FIELD] = prefix
-            r = cls._get_cropped_record(r, columns=columns, max_len=max_len)
-            yield formatter.format(**r)
+        return cls.get_display()._get_columnar_lines(records, columns, count, with_title, prefix, delimiter, max_len)
 
-    def output_table(
+    # @deprecated
+    def display_sheet(
             self,
             records: Iterable,
             columns: Sequence,
             count: AutoCount = None,
             with_title: bool = True,
+            output: AutoOutput = AUTO,
     ) -> None:
-        columnar_lines = self._get_columnar_lines(records, columns=columns, count=count, with_title=with_title)
-        for line in columnar_lines:
-            self.output_line(line)
+        return self.get_display().display_sheet(records, columns, count=count, with_title=with_title, output=output)
+
+    # @deprecated
+    def display_paragraph(self, paragraph: Iterable, level: Optional[int] = None, output=AUTO) -> None:
+        return self.get_display().display_paragraph(paragraph, level=level, output=output)
+
+    # @deprecated
+    def display_item(self, item, item_type='line', output=AUTO, **kwargs) -> None:
+        return self.get_display().display_item(item, item_type=item_type, output=output, **kwargs)
--- content/format/document_format.py
+++ content/format/document_format.py
@@ -1,36 +1,101 @@
-from typing import Optional, Iterable, Generator, Sequence
+from typing import Optional, Iterable, Generator, Sequence, Union
 
 try:  # Assume we're a submodule in a package.
     from interfaces import Item, ItemType, ContentType, AutoCount, Auto, AUTO
-    from base.mixin.line_output_mixin import LineOutputMixin, AutoOutput
+    from base.classes.display import DefaultDisplay, PREFIX_FIELD
+    from base.mixin.line_output_mixin import LineOutputMixin, AutoOutput, Class
     from utils.external import display, HTML, Markdown
-    from content.format.text_format import TextFormat
+    from content.format.text_format import TextFormat, Compress, DEFAULT_ENDING, DEFAULT_ENCODING
 except ImportError:  # Apparently no higher-level package has been imported, fall back to a local import.
     from ...interfaces import Item, ItemType, ContentType, AutoCount, Auto, AUTO
-    from ...base.mixin.line_output_mixin import LineOutputMixin, AutoOutput
+    from ...base.classes.display import DefaultDisplay, PREFIX_FIELD
+    from ...base.mixin.line_output_mixin import LineOutputMixin, AutoOutput, Class
     from ...utils.external import display, HTML, Markdown
-    from .text_format import TextFormat
+    from .text_format import TextFormat, Compress, DEFAULT_ENDING, DEFAULT_ENCODING
+
+H_STYLE = None
+P_STYLE = 'line-height: 1.1em; margin-top: 0em; margin-bottom: 0em; padding-top: 0em; padding-bottom: 0em;'
+SPACE = ' '
+HTML_SPACE = '&nbsp;'
 
 
-class DocumentFormat(TextFormat, LineOutputMixin):
-    def output_line(self, line: str, output: AutoOutput = AUTO) -> None:
-        if Auto.is_auto(output):
-            display(line)
+class DocumentFormat(TextFormat, DefaultDisplay):
+    def __init__(
+            self,
+            ending: str = DEFAULT_ENDING,
+            encoding: str = DEFAULT_ENCODING,
+            compress: Compress = None,
+    ):
+        self._current_paragraph = list()
+        super().__init__(ending=ending, encoding=encoding, compress=compress)
+
+    def get_current_paragraph(self) -> Iterable:
+        return self._current_paragraph
+
+    def set_current_paragraph(self, paragraph):
+        self._current_paragraph = paragraph
+        return self
+
+    def clear_current_paragraph(self):
+        return self.set_current_paragraph(list())
+
+    def get_encoded_paragraph(self, paragraph: Optional[Iterable] = None, level: Optional[int] = None, style=AUTO, clear: bool = False) -> str:
+        if paragraph:
+            if isinstance(paragraph, str):
+                paragraph = [paragraph]
+            for line in paragraph:
+                self.output_line(line)
+        encoded_paragraph = '\n'.join(self.get_current_paragraph())
+        if clear:
+            self.clear_current_paragraph()
+        return encoded_paragraph
+
+    @staticmethod
+    def _get_display_class():
+        return str
+
+    @classmethod
+    def _get_display_object(cls, data: str) -> str:
+        display_class = cls._get_display_class()
+        if display_class:
+            return display_class(data)
         else:
-            return super().output_line(line, output=output)
+            return str(data)
 
-    def output_paragraph(self, paragraph, level: Optional[int] = None, output: AutoOutput = AUTO):
-        self.output_line(str(paragraph), output=output)
+    def append_to_current_paragraph(self, line: str) -> None:
+        self._current_paragraph.append(line)
+
+    def output_line(self, line: str, output: AutoOutput = AUTO) -> None:
+        if line:
+            return self.append_to_current_paragraph(line)
+        else:
+            return self.display_paragraph(output=output)
+
+    # @deprecated
+    def get_output(self, output: AutoOutput = AUTO) -> Optional[Class]:
+        if Auto.is_auto(output):
+            return display
+        else:
+            return super().get_output(output=output)
+
+    def display_paragraph(self, paragraph: Optional[Iterable] = None, level: Optional[int] = None, style=AUTO, output: AutoOutput = AUTO) -> None:
+        if level:
+            self.display_paragraph(None, level=None, output=output)
+        data = self.get_encoded_paragraph(paragraph, level=level, style=style, clear=True)
+        if data:
+            obj = self._get_display_object(data)
+            display_method = self.get_output(output=output)
+            return display_method(obj)
 
 
 class MarkdownFormat(DocumentFormat):
-    def output_paragraph(self, paragraph, level: Optional[int] = None, output: AutoOutput = AUTO) -> None:
-        md_code = self.get_md_text_code(paragraph, level=level)
-        if Markdown:
-            md_obj = Markdown(md_code)
-        else:
-            md_obj = str(md_code)
-        return self.output_line(md_obj, output=output)
+    @staticmethod
+    def _get_display_class():
+        return Markdown
+
+    def get_encoded_paragraph(self, paragraph: Optional[Iterable] = None, level: Optional[int] = None, clear: bool = False) -> str:
+        data = super().get_encoded_paragraph(paragraph)
+        return self.get_md_text_code(data, level=level)
 
     @staticmethod
     def get_md_text_code(text: str, level: Optional[int] = None) -> str:
@@ -42,35 +107,50 @@ class MarkdownFormat(DocumentFormat):
 
 
 class HtmlFormat(DocumentFormat):
-    def output_paragraph(self, paragraph, level: Optional[int] = None, output: AutoOutput = AUTO) -> None:
-        html_code = self.get_html_text_code(paragraph, level=level)
-        if HTML:
-            html_obj = HTML(html_code)
-        else:
-            html_obj = str(html_code)
-        return self.output_line(html_obj, output=output)
+    @staticmethod
+    def _get_display_class():
+        return HTML
 
-    def output_table(
+    def get_encoded_paragraph(self, paragraph=None, level: Optional[int] = None, clear: bool = False, style=AUTO) -> str:
+        paragraph = super().get_encoded_paragraph(paragraph, clear=clear)
+        if paragraph:
+            html_code = self.get_html_text_code(paragraph, level=level, style=style)
+            return html_code.replace(SPACE * 2, HTML_SPACE * 2)
+        else:
+            return ''
+
+    def get_encoded_sheet(self, records, columns, count, with_title, style=AUTO) -> str:
+        columns = [c if isinstance(c, str) else c[0] for c in columns if c[0] != PREFIX_FIELD]  ###
+        html_code_lines = self.get_html_table_code(records, columns, count, with_title, style=style)
+        return '\n'.join(html_code_lines).replace(SPACE * 2, HTML_SPACE * 2)
+
+    def display_sheet(
             self,
             records: Iterable,
             columns: Sequence,
             count: AutoCount = None,
             with_title: bool = True,
+            style: Union[str, Auto, None] = AUTO,
             output: AutoOutput = AUTO,
     ) -> None:
-        html_code = '\n'.join(self.get_html_table_code(records, columns, count, with_title))
-        if HTML:
-            html_obj = HTML(html_code)
-        else:
-            html_obj = str(html_code)
-        return self.output_line(html_obj, output=output)
+        self.display_paragraph()
+        data = self.get_encoded_sheet(records, columns=columns, count=count, with_title=with_title, style=style)
+        display_obj = self._get_display_object(data)
+        display_method = self.get_output(output=output)
+        return display_method(display_obj)
 
     @staticmethod
-    def get_html_text_code(text: str, level: Optional[int] = None, style=None) -> str:
+    def get_html_text_code(text: Iterable, level: Optional[int] = None, style=AUTO) -> str:
+        if isinstance(text, str):
+            text = text.split('\n')
+        assert isinstance(text, Iterable), f'got {text}'
+        text = '<br>\n'.join(text)
         if level:
             tag = f'h{level}'
+            style = Auto.acquire(style, H_STYLE)
         else:
             tag = 'p'
+            style = Auto.acquire(style, P_STYLE)
         open_tag = f'<{tag} style="{style}">' if style else f'<{tag}>'
         close_tag = f'</{tag}>'
         return f'{open_tag}{text}{close_tag}'
@@ -81,17 +161,32 @@ class HtmlFormat(DocumentFormat):
             columns: Sequence,
             count: AutoCount = None,
             with_title: bool = True,
+            style: str = None,
     ) -> Generator:
         yield '<table>'
         if with_title:
+            yield '<thead>'
+            yield '<tr>'
             for c in columns:
                 yield f'<th>{c}</th>'
-        for n, r in enumerate(records):
-            if count:
+            yield '</tr>'
+        yield '</thead>'
+        yield '<tbody>'
+        for n, r in list(enumerate(records)):
+            if Auto.is_defined(count):
                 if n >= count:
                     break
             yield '<tr>'
-            for c in columns:
-                yield f'<td>{c}</td>'
+            for col in columns:
+                value = r.get(col)
+                if Auto.is_defined(style):
+                    yield f'<td>{value}</td>'
+                else:
+                    yield f'<td style="{style}">{value}</td>'
             yield '</tr>'
+        yield '</tbody>'
         yield '</table>'
+
+
+if HTML:
+    LineOutputMixin.display = HtmlFormat
