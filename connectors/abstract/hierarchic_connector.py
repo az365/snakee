03c23cc4e9ce1e4351ead41be5cca1a3b7f3be9a
az365 <artemcarr@yandex.ru>
2022-12-04 21:44:39 +0300

    HierarchicConnector: redundant imports removed

--- connectors/abstract/hierarchic_connector.py
+++ connectors/abstract/hierarchic_connector.py
@@ -1,13 +1,11 @@
 from abc import ABC
 from typing import Optional, Iterable
 
-try:  # Assume we're a sub-module in a package.
-    from utils import arguments as arg
+try:  # Assume we're a submodule in a package.
     from utils.decorators import deprecated_with_alternative
     from interfaces import AUTO, Auto, AutoBool, AutoContext, Connector, Class, Name
     from connectors.abstract.abstract_connector import AbstractConnector
 except ImportError:  # Apparently no higher-level package has been imported, fall back to a local import.
-    from ...utils import arguments as arg
     from ...utils.decorators import deprecated_with_alternative
     from ...interfaces import AUTO, Auto, AutoBool, AutoContext, Connector, Class, Name
     from .abstract_connector import AbstractConnector
@@ -56,7 +54,7 @@ class HierarchicConnector(AbstractConnector, ABC):
             child_class = cls.get_default_child_type().get_class(skip_missing=skip_missing)
         else:
             child_class = None
-        if not arg.is_defined(child_class):
+        if not Auto.is_defined(child_class):
             child_class = super().get_default_child_obj_class(skip_missing=skip_missing)
         return child_class
 
