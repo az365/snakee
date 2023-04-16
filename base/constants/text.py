2d79fd1123755e55c3d30f992056e2ba1a86a3ce
az365 <artemcarr@yandex.ru>
2023-04-16 14:40:47 +0300

    base.constants.text module added

---
+++ base/constants/text.py
@@ -0,0 +1,22 @@
+import re
+
+try:  # Assume we're a submodule in a package.
+    from base.constants.chars import EMPTY, DEFAULT_STR
+except ImportError:  # Apparently no higher-level package has been imported, fall back to a local import.
+    from ...base.constants.chars import EMPTY, DEFAULT_STR
+
+DEFAULT_ENCODING = 'utf8'
+
+LONG_LINE_LEN = 900
+JUPYTER_LINE_LEN = 120
+DEFAULT_LINE_LEN = JUPYTER_LINE_LEN
+EXAMPLE_STR_LEN = 12
+
+RE_LETTERS = re.compile('[^a-zа-я ]')
+NORM_LETTER_PAIRS = [('ё', 'е'), ]
+
+ZERO_VALUES = None, 'None', EMPTY, DEFAULT_STR, 0
+DEFAULT_TRUE_STR = 'Yes'
+DEFAULT_FALSE_STR = 'No'
+FALSE_VALUES = 'false', 'no', '0', '0.0', DEFAULT_STR, EMPTY
+STR_FALSE_SYNONYMS = ['False', 'None', 'none'] + list(FALSE_VALUES)  # 'false', 'no', '0', '0.0', DEFAULT_STR, EMPTY
