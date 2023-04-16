18f74f0a69eed888f0f188cb54565826b0ef2199
az365 <artemcarr@yandex.ru>
2023-04-16 23:27:46 +0300

    ColumnarFormat: POPULAR_DELIMITERS moved to base.constants.text

--- base/constants/text.py
+++ base/constants/text.py
@@ -1,9 +1,15 @@
 import re
 
 try:  # Assume we're a submodule in a package.
-    from base.constants.chars import EMPTY, DEFAULT_STR
+    from base.constants.chars import (
+        EMPTY, DEFAULT_STR,
+        TAB_CHAR, REQUEST_DELIMITER, ITEMS_DELIMITER, SEMICOLON, COMMA, SPACE,
+    )
 except ImportError:  # Apparently no higher-level package has been imported, fall back to a local import.
-    from ...base.constants.chars import EMPTY, DEFAULT_STR
+    from .chars import (
+        EMPTY, DEFAULT_STR,
+        TAB_CHAR, REQUEST_DELIMITER, ITEMS_DELIMITER, SEMICOLON, COMMA, SPACE,
+    )
 
 DEFAULT_ENCODING = 'utf8'
 
@@ -20,3 +26,4 @@ DEFAULT_TRUE_STR = 'Yes'
 DEFAULT_FALSE_STR = 'No'
 FALSE_VALUES = 'false', 'no', '0', '0.0', DEFAULT_STR, EMPTY
 STR_FALSE_SYNONYMS = ['False', 'None', 'none'] + list(FALSE_VALUES)  # 'false', 'no', '0', '0.0', DEFAULT_STR, EMPTY
+POPULAR_DELIMITERS = TAB_CHAR, REQUEST_DELIMITER, ITEMS_DELIMITER, SEMICOLON, COMMA, SPACE
